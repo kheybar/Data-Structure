04bf6a911ea8bb87b0f27f3bf3c69de8f8473930
mahdi zarepour <mahdizarepour15@gmail.com>
2022-06-17 16:39:30 +0430

    Dubly LinkedList, append

--- Doubly-Linked-List/Dubly-LinkedList.py
+++ Doubly-Linked-List/Dubly-LinkedList.py
@@ -11,9 +11,23 @@ class DublyLinkedList:
         self.head = new_node
         self.tail = new_node
         self.length = 1
+
+    def append(self, value):
+        new_node = Node(value)
+        self.tail.next = new_node
+        new_node.prev = self.tail
+        self.tail = new_node
+        self.length += 1
+        return True
     
     def print_list(self):
         temp = self.head
         while temp is not None:
             print(temp.value)
             temp = temp.next
+
+
+dll = DublyLinkedList(10)
+dll.append(12)
+dll.append(12)
+dll.print_list()
